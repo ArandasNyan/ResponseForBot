f623994b0322e776f1284b264946e67df3d30573
Arandas <99224016+ArandasNyan@users.noreply.github.com>
2023-06-25 14:48:15 -0300

    .

--- index.js
+++ index.js
@@ -1,57 +1,40 @@
-fetch('https://cherrybot.arandas.repl.co/status')
-    .then(response => response.json())
-    .then(data => {
-        let status = data.status || '';
-        const bodyElement = document.body;
-        const statusElement = document.getElementById('status');
+$(document).ready(function() {
+    (function atualizarStatus() {
+        $.ajax({
+            url: 'https://cherrybot.arandas.repl.co/status',
+            dataType: 'json',
+            success: function(data) {
+                let status = data.status || '';
+                const bodyElement = $('body');
+                const statusElement = $('#status');
 
-        if (status === '') {
-            bodyElement.classList.remove('error', 'success');
-            bodyElement.classList.add('pending');
-        } else if (status === 'Operacional!') {
-            bodyElement.classList.remove('error', 'pending');
-            bodyElement.classList.add('success');
-        } else {
-            bodyElement.classList.remove('pending', 'success');
-            bodyElement.classList.add('error');
-        }
+                if (status === '') {
+                    bodyElement.removeClass('error success').addClass('pending');
+                } else if (status === 'Operacional!') {
+                    bodyElement.removeClass('error pending').addClass('success');
+                } else {
+                    bodyElement.removeClass('pending success').addClass('error');
+                }
 
-        const array = ['Pendente', 'Pendente.', 'Pendente..', 'Pendente...'];
-        let currentIndex = 0; // Índice atual do item a ser exibido
+                const array = ['Pendente', 'Pendente.', 'Pendente..', 'Pendente...'];
+                let currentIndex = 0; // Índice atual do item a ser exibido
 
-        function exibirProximoItem() {
-            // Atualiza o conteúdo do elemento HTML com o item atual
-            const statusElement = document.getElementById('status');
-            statusElement.textContent = array[currentIndex];
+                function exibirProximoItem() {
+                    statusElement.text(array[currentIndex]);
+                    currentIndex = (currentIndex + 1) % array.length;
+                }
 
-            // Incrementa o índice para exibir o próximo item
-            currentIndex = (currentIndex + 1) % array.length;
-        }
+                const intervalo = 500; // 0,5 segundos
+                setInterval(exibirProximoItem, intervalo);
+            },
+            error: function(error) {
+                const bodyElement = $('body');
+                const statusElement = $('#status');
 
-        // Define o intervalo de tempo desejado (em milissegundos)
-        const intervalo = 500; // 5 segundos
-
-        status = setInterval(exibirProximoItem, intervalo);
-        statusElement.textContent = status;
-    })
-    .catch(error => {
-        const bodyElement = document.body;
-        const statusElement = document.getElementById('status');
-        statusElement.textContent = 'Não Operacional!';
-        bodyElement.classList.remove('pending', 'success');
-        bodyElement.classList.add('error');
-        console.error(error);
-    });
-/*
-// Faz uma requisição AJAX para obter o status do servidor
-                var xhr = new XMLHttpRequest();
-                xhr.open('GET', '/status', true);
-                xhr.onreadystatechange = function () {
-                    if (xhr.readyState === 4 && xhr.status === 200) {
-                        var statusElement = document.getElementById('status');
-                        var response = JSON.parse(xhr.responseText);
-                        statusElement.textContent = `Status: ${response.status}`;
-                    }
-                };
-                xhr.send();
-*/
+                statusElement.text('Não Operacional!');
+                bodyElement.removeClass('pending success').addClass('error');
+                console.error(error);
+            }
+        });
+    }());
+});
