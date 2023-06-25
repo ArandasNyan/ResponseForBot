c9fd711612b56910799a3fd54a62fa8dd204ad29
Arandas <99224016+ArandasNyan@users.noreply.github.com>
2023-06-25 19:16:39 -0300

    .

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 $(document).ready(function() {
-    (function atualizarStatus() {
+    let arrayIndex = 0;
+    const array = ['Pendente', 'Pendente.', 'Pendente..', 'Pendente...'];
+    let status = '';
+
+    const updateStatus = function() {
+        const bodyElement = $('body');
+        const statusElement = $('#status');
+
+        if (status === '') {
+            bodyElement.removeClass('error success').addClass('pending');
+            statusElement.text(array[arrayIndex]);
+            arrayIndex = (arrayIndex + 1) % array.length;
+        } else if (status === 'Operacional!') {
+            bodyElement.removeClass('error pending').addClass('success');
+            statusElement.text(status);
+            clearInterval(intervaloArray);
+        } else {
+            bodyElement.removeClass('pending success').addClass('error');
+            statusElement.text('Não Operacional!');
+            clearInterval(intervaloArray);
+        }
+    };
+
+    const fetchData = function() {
         $.ajax({
             url: 'https://cherrybot.arandas.repl.co/status',
             dataType: 'json',
             success: function(data) {
-                let status = data.status || '';
-                const bodyElement = $('body');
-                const statusElement = $('#status');
-
-                if (status === '') {
-                    bodyElement.removeClass('error success').addClass('pending');
-                } else if (status === 'Operacional!') {
-                    bodyElement.removeClass('error pending').addClass('success');
-                    statusElement.text(status); // Atualiza o status diretamente com o valor do JSON
-                    clearInterval(intervaloArray); // Limpa o intervalo de exibição do array
-                } else {
-                    bodyElement.removeClass('pending success').addClass('error');
-                    statusElement.text(status); // Atualiza o status diretamente com o valor do JSON
-                    clearInterval(intervaloArray); // Limpa o intervalo de exibição do array
-                }
+                status = data.status || '';
+                updateStatus();
             },
             error: function(error) {
-                const bodyElement = $('body');
-                const statusElement = $('#status');
-
-                statusElement.text('Não Operacional!');
-                bodyElement.removeClass('pending success').addClass('error');
+                status = '';
+                updateStatus();
                 console.error(error);
             }
         });
-    })();
+    };
+
+    fetchData();
 
     const intervaloAtualizacao = 5 * 1000; // 5 segundos
 
-    // Define o intervalo de atualização automática
-    setInterval(atualizarStatus, intervaloAtualizacao);
-
-    const array = ['Pendente', 'Pendente.', 'Pendente..', 'Pendente...'];
-    let currentIndex = 0; // Índice atual do item a ser exibido
-
-    function exibirProximoItem() {
-        const statusElement = $('#status');
-        statusElement.text(array[currentIndex]);
-        currentIndex = (currentIndex + 1) % array.length;
-    };
-
-    const intervaloArray = setInterval(exibirProximoItem, 500); 
+    const intervaloArray = setInterval(fetchData, intervaloAtualizacao);
 });
