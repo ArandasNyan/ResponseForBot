566cc0c1e251be1954534fe511bd491c161d794d
Arandas <99224016+ArandasNyan@users.noreply.github.com>
2023-06-26 17:57:27 -0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     let arrayIndex = 0;
     const array = ['Pendente', 'Pendente.', 'Pendente..', 'Pendente...'];
     let status = '';
-    let temporizadorDeVelocidade = 500;
 
     const updateStatus = function () {
         const bodyElement = $('body');
@@ -14,10 +13,12 @@ $(document).ready(function () {
             arrayIndex = (arrayIndex + 1) % array.length;
         } else if (status === 'Inoperante') {
             bodyElement.removeClass('success pending').addClass('error');
-            statusElement.text('inoperante!');
+            statusElement.text('Não Operacional!');
+            clearInterval(intervaloArray);
         } else {
             bodyElement.removeClass('pending error').addClass('success');
             statusElement.text('Operacional!');
+            clearInterval(intervaloArray);
         }
     };
 
@@ -25,28 +26,18 @@ $(document).ready(function () {
         $.ajax({
             url: 'https://cherrybot.arandas.repl.co/status',
             dataType: 'json',
-            timeout: temporizadorDeVelocidade,
+            timeout: 500,
             success: function (data) {
                 status = data.status || '';
                 updateStatus();
-                if (status === 'Operacional') {
-                    return; // Retorna sem agendar a próxima busca se estiver operacional
-                }
-                setTimeout(fetchData, temporizadorDeVelocidade); // Chama novamente após o tempo definido
             },
-            error: function (xhr, status, error) {
-                // Verifica o status da resposta e tenta lidar com possíveis erros
-                if (xhr.status === 0) {
-                    // Erro de conexão ou tempo limite
-                    console.log("Erro de conexão ou tempo limite");
-                } else if (xhr.status === 404) {
-                    // URL do endpoint não encontrada
-                    console.log("Endpoint não encontrado");
-                } else {
-                    // Outro erro de solicitação AJAX
-                    console.log("Erro na solicitação AJAX:", error);
-                }
-                setTimeout(fetchData, temporizadorDeVelocidade); // Chama novamente após o tempo definido
+            error: function (error) {
+                status = '';
+                updateStatus();
+                console.error(error);
+            },
+            complete: function () {
+                setTimeout(fetchData, 500); // Chama novamente após 500 milissegundos
             }
         });
     };
